fdd0d957f7221c8abd54fb9f467ca84cb1a987a4
Kathleen Catlett <kathleencatlett@comcast.net>
2017-01-15 17:38:33 -0500

    new database

--- assets/javascript/trainSchedule.js
+++ assets/javascript/trainSchedule.js
@@ -1,43 +1,41 @@
 
 
+ 
+
+
+
+  // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyBB7-HCnEi21ox3JuxWgjIHCmfFnUgPGBI",
-    authDomain: "train-schedule-66db2.firebaseapp.com",
-    databaseURL: "https://train-schedule-66db2.firebaseio.com",
-    storageBucket: "train-schedule-66db2.appspot.com",
-    messagingSenderId: "743138719065"
+    apiKey: "AIzaSyB5Bhg1V-_IWl8Irz5MNtsx_odFUCmf1So",
+    authDomain: "advanced-train-schedule.firebaseapp.com",
+    databaseURL: "https://advanced-train-schedule.firebaseio.com",
+    storageBucket: "advanced-train-schedule.appspot.com",
+    messagingSenderId: "820295676003"
   };
   firebase.initializeApp(config);
+var provider = new firebase.auth.GithubAuthProvider();
 
-  // var provider = new firebase.auth.GithubAuthProvider();
-  var provider = new firebase.auth.GithubAuthProvider();
-
-firebase.auth().getRedirectResult().then(function(result) {
-  console.log("authorization")
-  if (result.credential) {
-    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-    var token = result.credential.accessToken;
-    console.log("token - " + token);
-    // ...
-  }
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  console.log("authorization function");
+  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+  var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
-  console.log("user = " + user)
+  // ...
+
+  console.log("token + user = " + token + ": " + user)
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
 
-  console.log("error:  " + errorCode + ": " + errorMessage);
+  console.log("error:  " + errorCode + ":  " + errorMessage);
   // The email of the user's account used.
   var email = error.email;
   // The firebase.auth.AuthCredential type that was used.
   var credential = error.credential;
   // ...
 });
-
-
-
 
 
 
